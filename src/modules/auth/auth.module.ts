6c6120b1ff36a965d129774fad448b504dce1eb4
morgadodesarrollador <morgadodesarrollador@gmail.com>
2022-12-01 00:22:49 +0100

    Autenticacion: Passport/Jwt

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,12 +3,21 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [ 
-    TypeOrmModule.forFeature( [ User ])
+    TypeOrmModule.forFeature( [ User ]),
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: '1234',
+      signOptions: {
+        expiresIn: '2h'
+      }
+    })
   ],
   exports: [ TypeOrmModule, AuthService ]
 })
